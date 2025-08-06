22ceafc86dd5e792a9eb0440da1b3146e642f4ff
JosteinSandKjellsen <jostein.kjellsen@bouvet.no>
2025-08-06 18:20:36 +0200

    Refactor code structure for improved readability and maintainability

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XOmGtqH8kNqDKY8E5QflZLaXdb53vO7+iosXOQmNT5M=\"\n}"
--- .next/static/chunks/apps_base_src_app_layout_tsx_a0dd4e56._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules__pnpm_f057a8aa._.js",
-  "static/chunks/apps_base_src_app_layout_tsx_af4fefc4._.js",
-  "static/chunks/apps_base_src_app_globals_c6cd467a.css"
-],
-    source: "dynamic"
-});
